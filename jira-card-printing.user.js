3dc1291497af2bc26e8d0e20a384e2e90e383ac2
Wisen <wisen.tanasa@gmail.com>
2013-02-27 12:06:34 +0800

    Initial commit

---
+++ jira-card-printing.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        JIRA - Card print
+// @namespace   https://github.com/ceilfors
+// @description For printing JIRA cards, eliminating the unnecessaries, highlighting the necessary
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @include     http://eda-prod-hud02:*/jira/secure/Print.jspa*
+// ==/UserScript==
+
+// Make them big
+$("[id*='summary']").css('font-size', 30).children('div').css('overflow', 'visible').css('margin-bottom', '90px');
+$("[id*='customfield']").css('font-size', 20);
+$("[id*='assignee']").css('font-size', 20);
+
+// Enable print CSS
+$('body').css('-webkit-print-color-adjust', 'exact');
+
+// Remove not important stuff
+$("[id*='fixVersions']").hide();
+$("[id*='status']").hide();
+$("[id*='components']").hide();
+$("[id*='timeestimate']").hide();
+$("[id*='timeoriginalestimate']").hide();
+$("[id*='timespent']").hide();
+$("[id*='status']").hide();
+$("[id*='timespent']").hide();
+$('.gh-issue-inner').css('background-image', 'none');
+$('.gh-subhead').css('background-image', 'none');
+$('.gh-grabber').css('background-image', 'none');
