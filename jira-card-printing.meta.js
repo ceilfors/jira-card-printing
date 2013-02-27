574f88d03f4f65c47582e7fed55ae7ee744675b2
Wisen <wisen.tanasa@gmail.com>
2013-02-27 18:23:38 +0800

    Added updateURL and downloadURL

--- jira-card-printing.meta.js
+++ jira-card-printing.meta.js
@@ -1,3 +1,10 @@
 // ==UserScript==
-// @version 0.1
+// @name        JIRA - Card print
+// @namespace   https://github.com/ceilfors
+// @description For printing JIRA cards, eliminating the unnecessaries, highlighting the necessary
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @include     http://eda-prod-hud02:*/jira/secure/Print.jspa*
+// @updateURL	https://github.com/ceilfors/jira-card-printing/blob/master/jira-card-printing.meta.js
+// @downloadURL	https://github.com/ceilfors/jira-card-printing/blob/master/jira-card-printing.user.js
+// @version 	0.1
 // ==/UserScript==
